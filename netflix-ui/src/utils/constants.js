ed5a52e67b30b004248d1e50552d579a9843dc70
Rohitsaw6207 <rohitku6207@gmail.com>
2025-06-09 12:03:24 +0530

    change the required changes

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9";
+export const API_KEY = "85ca1b9385305dc6d3a58c9179e47591";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -2,14 +2,16 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1Hf0_rdWLBzDPJPcO9CJN4y6M6-EgKH4",
-  authDomain: "react-auth-6788d.firebaseapp.com",
-  projectId: "react-auth-6788d",
-  storageBucket: "react-auth-6788d.appspot.com",
-  messagingSenderId: "131797845021",
-  appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-  measurementId: "G-VWPBR1NSLL",
+  apiKey: "AIzaSyBy6C0s-vBZk9ZZw9i_Bmmo4kHM490LXCo",
+  authDomain: "netfilx-clone-c8d27.firebaseapp.com",
+  projectId: "netfilx-clone-c8d27",
+  storageBucket: "netfilx-clone-c8d27.firebasestorage.app",
+  messagingSenderId: "651575222832",
+  appId: "1:651575222832:web:52874ef5d9e544158083f5",
+  measurementId: "G-XWDM03CB6B"
 };
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
+
+
